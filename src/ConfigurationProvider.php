a4302c5f7823958048606be4ae000a8648708e03
Mohamadreza <mreza@Mohamadrezas-MacBook-Pro.local>
2023-01-11 00:03:20 +0100

    implement bootable in the configuration provider

--- src/ConfigurationProvider.php
+++ src/ConfigurationProvider.php
@@ -2,17 +2,17 @@
 
 namespace Weebel\Configuration;
 
+use Weebel\Contracts\Bootable;
 use Weebel\Contracts\Container;
 
-class ConfigurationProvider
+class ConfigurationProvider implements Bootable
 {
     public function __construct(protected Container $container)
     {
     }
 
-    public function __invoke(): void
+    public function boot(): void
     {
         $this->container->alias(\Weebel\Contracts\Configuration::class, Configuration::class);
     }
-
 }
